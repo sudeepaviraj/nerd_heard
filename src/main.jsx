493cbb410b04913bdc5ed0c7a866dba88a7ad8f5
Sudeep Virajith <virajithaabekoon@gmail.com>
2023-08-28 21:09:35 +0530

    added routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LoginScreen from './components/LoginScreen.jsx'
+import ScanScreen from './components/ScanScreen.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<LoginScreen/>}/>
+    <Route path='/home' element={<ScanScreen/>}/>
+  </Routes>
+  </BrowserRouter>,
 )
