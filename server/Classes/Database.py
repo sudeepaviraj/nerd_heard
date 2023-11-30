14ce9b69650267fa33ebbf73a571fc26da3bee5e
Sudeep Virajith <virajithaabekoon@gmail.com>
2023-11-30 15:14:54 +0530

    classes initialized

--- server/Classes/Database.py
+++ server/Classes/Database.py
@@ -1,5 +1,21 @@
 import sqlite3
-con = sqlite3.connect("users.db")
-cur = con.cursor()
-cur.execute("""INSERT INTO user(user_name,index_no,reg_no,lecture_group,lab_group) VALUES ("ABEKOON A M S V","ICT/2/801","TE110426","I1","I1S")""")
-con.commit()
+class Connection:
+    connection = sqlite3.connect("users.db")
+    curser = connection.cursor()
+    def __init__(self) -> None:
+        pass
+    
+    def Insert(self,sql:str)->bool:
+        self.curser.execute(sql)
+        try:
+            self.connection.commit()
+            return True
+        except Exception as e:
+            return False   
+    
+    def Fetch(self,sql:str):
+        res = self.curser.execute(sql)
+        try:
+            return res.fetchall()
+        except Exception as e:
+            return e
---
+++ server/Classes/User.py
@@ -0,0 +1,8 @@
+
+class User:    
+    def __init__(self) -> None:
+        pass
+    def Login(self,reg_no,index):
+        pass
+    def MarkAttendance(self):
+        pass
